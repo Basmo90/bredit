9371392b9cd6acfe7a78455042e7dc26495e33a2
Basmo90 <129325758+Basmo90@users.noreply.github.com>
2024-11-24 19:26:43 +0000

    removed proxy and reverted back to reddit url rather than proxy url

--- src/components/api.js
+++ src/components/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const PROXY_URL = 'http://localhost:5000/api/reddit';
-
-//const REDDIT_API_URL = 'https://www.reddit.com';
+const REDDIT_API_URL = 'https://www.reddit.com';
 
 export const fetchRedditPosts = async (query = '') => {
     try {
-        const url = query ? `${PROXY_URL}/search.json?q=${query}&restrict_sr=1` : `${PROXY_URL}/.json`;
+        const url = query ? `${REDDIT_API_URL}/search.json?q=${query}&restrict_sr=1` : `${REDDIT_API_URL}/.json`;
         const response = await axios.get(url);
         
         console.log('API Response: ', response.data);
@@ -22,7 +20,7 @@ export const fetchRedditPosts = async (query = '') => {
 
 export const fetchRedditComments = async (postId) => {
     try {
-        const response = await axios.get(`${PROXY_URL}${postId}.json`);
+        const response = await axios.get(`${REDDIT_API_URL}${postId}.json`);
         const comments = response.data[1].data.children.slice(0,10);
         return comments;
     } catch (error) {
